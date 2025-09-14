6b1d4ea55a0e8fba85b4a9c09458f2e33ae56ed8
Avins VR <avins2005@gmail.com>
2025-09-14 22:46:54 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],   // ✅ ensures React is injected
-  base: "./",
+  plugins: [react()],
+  base: "./",   // ✅ ensures relative paths
   build: {
     outDir: "dist"
   }
